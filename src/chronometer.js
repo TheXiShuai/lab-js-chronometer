c1aaba181d030787b648ff385d9bb3e866e32636
John Doe <johndoe@example.com>
2023-07-18 17:51:44 +0200

    lab chrono

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,57 @@
 class Chronometer {
   constructor() {
+    this.currentTime = 0;
+    this.intervalId = null;
     // ... your code goes here
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+      this.currentTime += 1;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    // let newCurrentTime = this.currentTime.toString()
+    // if(newCurrentTime.length === 1){
+    //   return "0" + newCurrentTime
+    // }
+    // else{
+    //   return newCurrentTime
+    // }
+    // }
+    if (value <= 9) {
+      return "0" + value;
+    } else {
+      return value.toString();
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return (
+      this.computeTwoDigitNumber(this.getMinutes()) +
+      ":" +
+      this.computeTwoDigitNumber(this.getSeconds())
+    );
   }
+
 }
